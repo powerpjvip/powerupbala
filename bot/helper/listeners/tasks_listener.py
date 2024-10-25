7e0034a62e0368653bdb5a99ef9af5b4b97c01f7
powerpjvip <154010091+powerpjvip@users.noreply.github.com>
2024-10-25 06:52:30 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -563,7 +563,7 @@ class MirrorLeechListener:
                             buttons.ubutton(BotTheme('INDEX_LINK_D'), share_url)
                             buttons.ubutton(BotTheme('VIEW_LINK'), share_urls)
 
-                buttons = extra_btns(buttons)
+                #buttons = extra_btns(buttons)
                 #button = buttons.build_menu(2)
             else:
                 msg += BotTheme('RCPATH', RCpath=rclonePath)
