0cf27c625834f941ca50f116a3d13673b6f8f21b
powervip <154010091+powerpjvip@users.noreply.github.com>
2025-06-03 19:29:00 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -24,6 +24,8 @@ from uvloop import install
 install()
 setdefaulttimeout(600)
 
+from pyrogram import utils as pyroutils
+
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
 botStartTime = time()
